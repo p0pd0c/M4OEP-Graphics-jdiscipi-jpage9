60bb5c58e866c0481239c69a82133a46da213f79
Jared DiScipio <jared.michael.discipio@gmail.com>
2021-04-30 18:03:46 -0400

    First Commit

---
+++ cmake-build-debug/CMakeFiles/graphics.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jareddiscipio/Desktop/M4OE-jdiscipi-jpage9/circle.cpp" "/Users/jareddiscipio/Desktop/M4OE-jdiscipi-jpage9/cmake-build-debug/CMakeFiles/graphics.dir/circle.cpp.o"
+  "/Users/jareddiscipio/Desktop/M4OE-jdiscipi-jpage9/graphics.cpp" "/Users/jareddiscipio/Desktop/M4OE-jdiscipi-jpage9/cmake-build-debug/CMakeFiles/graphics.dir/graphics.cpp.o"
+  "/Users/jareddiscipio/Desktop/M4OE-jdiscipi-jpage9/shape.cpp" "/Users/jareddiscipio/Desktop/M4OE-jdiscipi-jpage9/cmake-build-debug/CMakeFiles/graphics.dir/shape.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.0.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
